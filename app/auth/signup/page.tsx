bbf19be4a6910c12054cd9daeaf173ef2ba99a5c
xxecond <andrewsampadu9@gmail.com>
2026-05-17 01:18:16 +0000

    update 3

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -10,7 +10,7 @@ export default function signUpPage(){
             <main className="flex relative rounded-4xl overflow-hidden h-full max-h-4/6 max-w-5/6 w-full   bg-yellow-600">
                 <section className="w-1/2 hidden lg:block bg-black" ></section>
                 <section className="lg:w-1/2 w-full bg-white flex justify-center items-center"><SignupForm
-                 className="text-black  w-5/6" /></section>
+                 /></section>
             </main>
         </div>
     )
